45eed5d3a72cd4afe8a15b4d44a38a2a666d8877
«ChulakovEvgeniy» <>
2023-07-30 21:34:20 +0300

    сделано 3 дз

--- src/main/java/familyTree/Main.java
+++ src/main/java/familyTree/Main.java
@@ -10,18 +10,18 @@ import java.io.IOException;
 import java.time.LocalDate;
 
 public class Main {
-//    На момент написания дз урок2 не смотрел
+    //    На момент написания дз урок2 не смотрел
     public static void main(String[] args) throws IOException {
-        Human evgeniy = new Human("Евгений","Чулаков","Игоревич",
-                LocalDate.of(1995,11,05), Gender.Male);
-        Human igor = new Human("Игорь","Чулаков","Иванович",
-                LocalDate.of(1971,12,27),Gender.Male);
-        Human helga = new Human("Ольга","Чулакова","Игоревна",
-                LocalDate.of(2005,02,05),Gender.Female);
-        Human vitaliy = new Human("Виталий","Рула","Федорович",
-                LocalDate.of(1956,02,3), LocalDate.of(2021,12,2),Gender.Male);
-        Human natalya = new Human("Наталья","Чулакова","Александровна",
-                LocalDate.of(1972,12,30),Gender.Female);
+        Human evgeniy = new Human("Евгений", "Чулаков", "Игоревич",
+                LocalDate.of(1995, 11, 05), Gender.Male);
+        Human igor = new Human("Игорь", "Чулаков", "Иванович",
+                LocalDate.of(1971, 12, 27), Gender.Male);
+        Human helga = new Human("Ольга", "Чулакова", "Игоревна",
+                LocalDate.of(2005, 02, 05), Gender.Female);
+        Human vitaliy = new Human("Виталий", "Рула", "Федорович",
+                LocalDate.of(1956, 02, 3), LocalDate.of(2021, 12, 2), Gender.Male);
+        Human natalya = new Human("Наталья", "Чулакова", "Александровна",
+                LocalDate.of(1972, 12, 30), Gender.Female);
         evgeniy.setFather(igor);
         natalya.addChild(evgeniy);
         helga.setFather(igor);
@@ -39,10 +39,6 @@ public class Main {
         System.out.println(tree);
         tree.sortByAge();
         System.out.println(tree);
-
-
-
-
 
 
     }
--- src/main/java/familyTree/familyTree/FamilyTRee.java
+++ src/main/java/familyTree/familyTree/FamilyTRee.java
@@ -9,35 +9,39 @@ import java.util.Comparator;
 import java.util.Iterator;
 import java.util.List;
 
-public class FamilyTRee implements Serializable,Iterable<Human> {
-        private List<Human> famTree;
-        public FamilyTRee() {
-            this.famTree = new ArrayList<>();
-        }
-        public void addfam(Human human){
-            if (!famTree.contains(human)){
-                famTree.add(0,human);
-            }
-            if (!famTree.contains(human.getFather())) {
-                if (human.getFather() != null) {
-                    famTree.add(0, human.getFather());
-                }
-            }
-            if (!famTree.contains(human.getMather())) {
-                if (human.getMather() != null) {
-                    famTree.add(0, human.getMather());
-                }
-            }
+public class FamilyTRee implements Serializable, Iterable<Human> {
+    private List<Human> famTree;
 
-        }
-    public String getInfo(){
-            StringBuilder sb = new StringBuilder();
-            for (Human human: famTree){
-                sb.append(human);
-                sb.append("\n");
-            }
-            return sb.toString();
+    public FamilyTRee() {
+        this.famTree = new ArrayList<>();
     }
+
+    public void addfam(Human human) {
+        if (!famTree.contains(human)) {
+            famTree.add(0, human);
+        }
+        if (!famTree.contains(human.getFather())) {
+            if (human.getFather() != null) {
+                famTree.add(0, human.getFather());
+            }
+        }
+        if (!famTree.contains(human.getMather())) {
+            if (human.getMather() != null) {
+                famTree.add(0, human.getMather());
+            }
+        }
+
+    }
+
+    public String getInfo() {
+        StringBuilder sb = new StringBuilder();
+        for (Human human : famTree) {
+            sb.append(human);
+            sb.append("\n");
+        }
+        return sb.toString();
+    }
+
     @Override
     public String toString() {
         return getInfo();
@@ -47,10 +51,12 @@ public class FamilyTRee implements Serializable,Iterable<Human> {
     public Iterator<Human> iterator() {
         return new HumanIterator(famTree);
     }
-    public void sortByName(){
-            famTree.sort((Comparator<? super Human>) new HumanComparatorByName());
+
+    public void sortByName() {
+        famTree.sort((Comparator<? super Human>) new HumanComparatorByName());
     }
-    public void sortByAge(){
+
+    public void sortByAge() {
         famTree.sort((Comparator<? super Human>) new HumanComparatorByAge());
     }
 }
